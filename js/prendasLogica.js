bb3220ef96bc983f0c6d9e98438c920cb1a63531
NachiLopez <nazarenolopez35@gmail.com>
2023-07-28 19:34:51 -0300

    Se elimino todos los input select que simplemente se escondian dependiendo de la categoria de prenda seleccionada, ahora solo hay un input select con el label 'nombre de prenda' y sus opciones se modifican dependiendo de la categoria seleccionada, ademas se agrego un archivo JSON para simular una BD donde estan guardados las prendas. Tambien se agrego el atributo precio a las prendas y se agrego sweet alert al proyecto para agregarle algo mas de estetica.

--- js/prendasLogica.js
+++ js/prendasLogica.js
@@ -1,28 +1,49 @@
-function agregarPrendas() {
-  // Busco el nombre de la persona y la prenda para guardarlo.
-  const nombrePersona = document.getElementById('inputNombre').value;
+let prendasDisponibles = [];
+
+async function obtenerPrendasDisponibles() {
+  try {
+    const response = await fetch('../jsons/prendasDisponibles.json');
+    if (!response.ok) {
+      throw new Error('Error al obtener archivo JSON');
+    }
+    prendasDisponibles = await response.json();
+    return prendasDisponibles;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+}
+
+function actualizarPrendasDisponibles() {
   const tipoPrenda = document.getElementById('inputPrenda').value;
-  const nombrePrenda = obtenerNombrePrenda();
+  const inputNombrePrenda = document.getElementById('inputNombrePrenda');
+  inputNombrePrenda.innerHTML = '';
 
-  if (nombrePersona && tipoPrenda && nombrePrenda) {
-    const listaPrendas = document.getElementById('listaPrendas');
-    const nuevaPrenda = document.createElement('li');
-    nuevaPrenda.textContent = `Comprador: ${nombrePersona}. Tipo de prenda: ${tipoPrenda}. Nombre de la prenda: ${nombrePrenda}`;
-    const btnEliminar = document.createElement('button');
-      btnEliminar.textContent = 'Eliminar';
-      btnEliminar.addEventListener('click', () => eliminarPrenda(btnEliminar));
-      nuevaPrenda.appendChild(btnEliminar);
-      listaPrendas.appendChild(nuevaPrenda);
+  const optionEmpty = document.createElement('option');
+  optionEmpty.value = 'empty';
+  optionEmpty.textContent = 'Seleccionar';
+  inputNombrePrenda.appendChild(optionEmpty);
 
-    // Guardar datos en el Storage
-    const prendasGuardadas = localStorage.getItem('prendas');
-    let prendas = prendasGuardadas ? JSON.parse(prendasGuardadas) : [];
-    prendas.push({
-      nombrePersona,
-      tipoPrenda,
-      nombrePrenda
+  if (tipoPrenda != null) {
+    const prendasFiltradas = prendasDisponibles.filter(prenda => prenda.categoria == tipoPrenda);
+    const precioPrenda = document.getElementById("precioPrenda");
+
+    for (const prenda of prendasFiltradas) {
+      const option = document.createElement('option');
+      option.value = prenda.nombre;
+      option.textContent = prenda.nombre;
+      inputNombrePrenda.appendChild(option);
+    }
+
+    inputNombrePrenda.addEventListener('change', () => {
+      const selectedPrenda = inputNombrePrenda.value;
+      const prendaSeleccionada = prendasDisponibles.find(prenda => prenda.nombre == selectedPrenda);
+      if (prendaSeleccionada != null) {
+        precioPrenda.textContent = `Precio: ${prendaSeleccionada.precio}`;
+      } else {
+        precioPrenda.textContent = 'Precio: ';
+      }
     });
-    localStorage.setItem('prendas', JSON.stringify(prendas));
   }
 }
 
@@ -34,7 +55,7 @@ function cargarPrendasGuardadas() {
     listaPrendas.innerHTML = ''
     prendas.forEach((prenda) => {
       const nuevaPrenda = document.createElement('li');
-      nuevaPrenda.textContent = `Comprador: ${prenda.nombrePersona}. Tipo de prenda: ${prenda.tipoPrenda}. Nombre de la prenda: ${prenda.nombrePrenda}`;
+      nuevaPrenda.textContent = `Comprador: ${prenda.nombrePersona}. Tipo de prenda: ${prenda.tipoPrenda}. Nombre de la prenda: ${prenda.nombrePrenda}. Precio: ${prenda.precioPrenda}`;
       const btnEliminar = document.createElement('button');
       btnEliminar.textContent = 'Eliminar';
       btnEliminar.addEventListener('click', () => eliminarPrenda(btnEliminar));
@@ -44,45 +65,113 @@ function cargarPrendasGuardadas() {
   }
 }
 
-function eliminarPrenda(btnEliminar) {
-  const listaPrendas = document.getElementById('listaPrendas');
-  const indice = Array.prototype.indexOf.call(listaPrendas.children, btnEliminar.parentNode);
+function agregarPrendas() {
+  const nombrePersona = document.getElementById('inputNombre').value;
+  const tipoPrenda = document.getElementById('inputPrenda').value;
+  const nombrePrenda = document.getElementById('inputNombrePrenda').value;
 
-  if (indice != -1) {
+  if (nombrePersona == '') {
+    mostrarAlert("falta", "ingresar nombre");
+  } else if (tipoPrenda == 'empty') {
+    mostrarAlert("falta", "seleccionar categoría de prenda");
+  } else if (nombrePrenda == 'empty') {
+    mostrarAlert("falta", "seleccionar una prenda")
+  }
+  if (nombrePersona != '' && tipoPrenda != 'empty' && nombrePrenda != 'empty') {
+    const precioPrenda = prendasDisponibles.find(prenda => prenda.nombre == nombrePrenda).precio;
+    const listaPrendas = document.getElementById('listaPrendas');
+    const nuevaPrenda = document.createElement('li');
+    nuevaPrenda.textContent = `Comprador: ${nombrePersona}. Tipo de prenda: ${tipoPrenda}. Nombre de la prenda: ${nombrePrenda}. Precio: ${precioPrenda}`;
+    const btnEliminar = document.createElement('button');
+    btnEliminar.textContent = 'Eliminar';
+    btnEliminar.addEventListener('click', () => eliminarPrenda(btnEliminar));
+    nuevaPrenda.appendChild(btnEliminar);
+    listaPrendas.appendChild(nuevaPrenda);
+
+
     const prendasGuardadas = localStorage.getItem('prendas');
-    const prendas = prendasGuardadas ? JSON.parse(prendasGuardadas) : [];
-
-    prendas.splice(indice, 1);
+    let prendas = prendasGuardadas ? JSON.parse(prendasGuardadas) : [];
+    prendas.push({
+      nombrePersona,
+      tipoPrenda,
+      nombrePrenda,
+      precioPrenda
+    });
     localStorage.setItem('prendas', JSON.stringify(prendas));
-    cargarPrendasGuardadas();
-
-    btnEliminar.parentNode.remove();
+    mostrarAlert("guardado");
   }
 }
 
-function obtenerNombrePrenda() {
-  const prenda = document.getElementById('inputPrenda').value;
-  let nombrePrenda = '';
+async function eliminarPrenda(btnEliminar) {
+  const listaPrendas = document.getElementById('listaPrendas');
+  const indice = Array.prototype.indexOf.call(listaPrendas.children, btnEliminar.parentNode);
+  const prendasGuardadas = localStorage.getItem('prendas');
+  const prendas = prendasGuardadas ? JSON.parse(prendasGuardadas) : [];
+  const confirmDelete = await mostrarAlert("eliminar");
+  if (confirmDelete.isConfirmed) {
+    if (indice != -1) {
 
-  switch (prenda) {
-    case 'Camisetas':
-      nombrePrenda = document.getElementById('inputNombreCamiseta').value;
-      break;
-    case 'Hoddies':
-      nombrePrenda = document.getElementById('inputNombreHoddie').value;
-      break;
-    case 'Camperas':
-      nombrePrenda = document.getElementById('inputNombreCampera').value;
-      break;
-    case 'Pantalones':
-      nombrePrenda = document.getElementById('inputNombrePantalon').value;
-      break;
-    default:
-      break;
+      prendas.splice(indice, 1);
+      localStorage.setItem('prendas', JSON.stringify(prendas));
+      cargarPrendasGuardadas();
+
+      btnEliminar.parentNode.remove();
+    }
   }
-  return nombrePrenda;
+}
+
+function mostrarAlert(opt, info) {
+  return new Promise((resolve, reject) => {
+    switch (opt) {
+      case "falta":
+        Swal.fire({
+          title: `<strong>Falta ${info}</strong>`,
+          icon: 'info',
+          html: 'Completa los campos vacios antes de <b>guardar</b>.',
+          showCloseButton: true,
+          showCancelButton: false,
+          focusConfirm: false,
+          confirmButtonText: '<i class="fa fa-thumbs-up"></i> Aceptar',
+          cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
+          cancelButtonAriaLabel: 'Thumbs down'
+        })
+        break;
+
+      case "guardado":
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'La compra ha sido realizada',
+          showConfirmButton: false,
+          timer: 1200
+        })
+        break;
+
+      case "eliminar":
+        confirmacion();
+
+        function confirmacion() {
+          Swal.fire({
+            title: 'Estas seguro de eliminar esta prenda?',
+            text: "No vas a poder recuperar la compra!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Borrar'
+          }).then((result) => {
+            resolve(result);
+          });
+        }
+        break;
+    }
+  });
 }
 
 document.getElementById('btnAgregar').addEventListener('click', agregarPrendas);
 
-window.addEventListener('load', cargarPrendasGuardadas);
+window.addEventListener('load', () => {
+  obtenerPrendasDisponibles()
+  actualizarPrendasDisponibles();
+  cargarPrendasGuardadas();
+});
